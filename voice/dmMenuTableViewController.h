a464daff6d634a343773289f2c8649c2d4c46164
Dmitry Maklygin <d.maklygin@gmail.com>
2014-06-27 02:32:44 +0400

    Отображение для левого меню наследуется от TableVC

---
+++ voice/dmMenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  dmMenuTableViewController.h
+//  voice
+//
+//  Created by Дмитрий on 27.06.14.
+//  Copyright (c) 2014 Дмитрий. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dmMenuTableViewController : UITableViewController
+
+@end
